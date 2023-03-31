9f2dbe94a5ed316901ce6ae2c9d0371dabce79a4
JopeOliveira23 <jope4256@gmail.com>
2023-03-31 11:12:20 -0300

    30/03
    lista css

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,13 @@
+import './styles.css';
+
 function Home() {
 
   return (
-    <>
+    <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite seu nome..."/>
       <button type="button">Adicionar</button>
-    </>
+    </div>
   )
 }
 
